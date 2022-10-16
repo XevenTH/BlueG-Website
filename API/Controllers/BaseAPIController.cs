6bce1e19cc726aaa34c0f443d4ede0200b97dd88
Angga <emmanuelangga@gmail.com>
2022-10-16 13:45:50 +0700

    Walking Skeleton API 1

---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BaseAPIController : ControllerBase
+    {
+        
+    }
+}
